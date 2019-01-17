7a7f05c018db2b48fd8873c3fb5cead6d1f4ac3f
Stefan Oberleitner <stefan@dhcp-10-181-33-94.dynamic.eduroam.mwn.de>
2019-01-17 10:29:54 +0100

    Create folder structure and entry point of GUI

---
+++ gui/gui.py
@@ -0,0 +1,14 @@
+import wx
+from .welcomeScreen import WelcomeScreen
+
+
+def osdriaApp():
+    """Run GUI"""
+    osdria = wx.App()
+    screen = WelcomeScreen()
+    screen.Show()
+    osdria.MainLoop()
+
+
+if __name__ == '__main__':
+    osdriaApp()
